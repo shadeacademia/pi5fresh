20cad66c8450a0e656969c6222063c5436d1ac65
shadeacademia <93976546+shadeacademia@users.noreply.github.com>
2024-05-29 15:42:06 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt -o Apt::Get::Assume-Yes=true remove libreoffice* thunderbird && sudo ap
 
 sudo apt update && sudo apt -o Apt::Get::Assume-Yes=true dist-upgrade
 
-sudo apt -o Apt::Get::Assume-Yes=true install ffmpeg g++ gcc gimp git krita libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio openjdk-8-jre openjdk-17-jre openjdk-21-jre python3-dev python3-pip retroarch vlc vulkan-tools wget zlib1g-dev
+sudo apt -o Apt::Get::Assume-Yes=true install deluge ffmpeg g++ gcc gimp git krita libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio openjdk-8-jre openjdk-17-jre openjdk-21-jre python3-dev python3-pip retroarch vlc vulkan-tools wget zlib1g-dev
 
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
