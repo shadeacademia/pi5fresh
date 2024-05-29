318f3f693fc1e0e9abc83d7142014bd0a639fb64
shadeacademia <93976546+shadeacademia@users.noreply.github.com>
2024-05-29 15:28:47 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ sudo apt -o Apt::Get::Assume-Yes=true remove libreoffice* thunderbird && sudo ap
 
 sudo apt update && sudo apt -o Apt::Get::Assume-Yes=true dist-upgrade
 
-wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
+sudo apt -o Apt::Get::Assume-Yes=true install ffmpeg g++ gcc gimp git krita libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make obs-studio openjdk-8-jre openjdk-17-jre openjdk-21-jre python3-dev python3-pip retroarch vlc wget zlib1g-dev
 
-sudo apt -o Apt::Get::Assume-Yes=true install ffmpeg g++ gcc gimp git krita libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make obs-studio openjdk-8-jre openjdk-17-jre openjdk-21-jre python3-dev python3-pip retroarch vlc zlib1g-dev
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
 cd ~
 
