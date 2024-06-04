1f93edea0dff1bd34a3ee348cc784190dfa73f7e
shadeacademia <93976546+shadeacademia@users.noreply.github.com>
2024-06-05 02:19:33 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt -o Apt::Get::Assume-Yes=true remove libreoffice* transmission-gtk && su
 
 sudo apt update && sudo apt -o Apt::Get::Assume-Yes=true dist-upgrade
 
-sudo apt -o Apt::Get::Assume-Yes=true install deluge ffmpeg g++ gcc gimp git krita libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio python3-dev python3-pip retroarch vlc vulkan-tools wget zlib1g-dev
+sudo apt -o Apt::Get::Assume-Yes=true install cmake deluge ffmpeg g++ gcc gimp git krita libcurl4-openssl-dev libfontconfig1-dev libgl1-mesa-dev libglew-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libvulcan-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio python3-dev python3-pip retroarch vlc vulkan-tools wget zlib1g-dev
 
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 
