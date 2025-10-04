e598d8ac8ebe003403a169631c5e050299d4e1a0
shadeacademia <93976546+shadeacademia@users.noreply.github.com>
2025-10-04 13:17:12 +1000

    Update setup.sh
    ...remove default-jre and add flatpak

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt -o Apt::Get::Assume-Yes=true dist-upgrade
 
-sudo apt -o Apt::Get::Assume-Yes=true install cmake curl default-jre ffmpeg g++ gcc gimp git glmark2 krita libcurl4-openssl-dev libfontconfig1-dev libgl1-mesa-dev libglew-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio python3-dev python3-pip qbittorrent retroarch vlc vulkan-tools wget zlib1g-dev
+sudo apt -o Apt::Get::Assume-Yes=true install cmake curl ffmpeg flatpak g++ gcc gimp git glmark2 krita libcurl4-openssl-dev libfontconfig1-dev libgl1-mesa-dev libglew-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libxml2-dev libxslt1-dev make mesa-vulkan-drivers obs-studio python3-dev python3-pip qbittorrent retroarch vlc vulkan-tools wget zlib1g-dev
 
 cd ~
 
